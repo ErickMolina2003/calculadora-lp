e5c9b62681a5774f2cceb76284cfc9e0f4d2fd5b
Lleymi <lleymicruz18@gmail.com>
2022-08-08 13:48:06 -0600

    Operaciones

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
 console.log('hola');
+function digitar_operacion(num){
+    document.getElementById('principal').value+=num
+    console.log(document.getElementById('principal').value)
+}
+function limpiar(){
+    document.getElementById('principal').value=null
+}
+var num1
+var operador
+function operacion(op){
+    num1=document.getElementById('principal').value
+    limpiar()
+    console.log(op)
+    operador=op
+}
+var num2
+function igual(){
+    if(operador=='+'){
+        num2=document.getElementById('principal').value
+        limpiar()
+        document.getElementById('principal').value=Number(num1) + Number(num2)
+    }
+    if(operador=='-'){
+        num2=document.getElementById('principal').value
+        limpiar()
+        document.getElementById('principal').value=Number(num1) - Number(num2)
+    }
+    if(operador=='*'){
+        num2=document.getElementById('principal').value
+        limpiar()
+        document.getElementById('principal').value=Number(num1) * Number(num2)
+    }
+    if(operador=='/'){
+        num2=document.getElementById('principal').value
+        limpiar()
+        document.getElementById('principal').value=Number(num1) / Number(num2)
+    }
+}
