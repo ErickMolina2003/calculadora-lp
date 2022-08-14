90852be7305013c2489a2d48c43914e8c016f6a3
erick molina <eralejo2003@gmail.com>
2022-08-13 23:28:28 -0600

    main js Erick

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # calculadora-lp
+
+1) Erick Alejandro Molina Garcia 20201002723
+2) Lleymi Montoya
+3) Angie Arias
--- main.js
+++ main.js
@@ -1,39 +1,77 @@
-console.log('hola');
-function digitar_operacion(num){
-    document.getElementById('principal').value+=num
-    console.log(document.getElementById('principal').value)
+console.log("Nota: Las operaciones son realizadas por orden, no por jerarquia de operaciones");
+
+var valorActual = '';
+var numeros = [];
+var resultado;
+var valorPantalla = '';
+
+function limpiar() {
+    document.getElementById('principal').value = '';
+    numeros = [];
+    resultado = '';
+    valorActual = '';
+    valorPantalla = '';
 }
-function limpiar(){
-    document.getElementById('principal').value=null
+
+function digitar_operacion(num) {
+    valorPantalla += `${num}`;
+    valorActual = valorActual + `${num}`;
+    document.getElementById('principal').value = valorPantalla;
 }
-var num1
-var operador
-function operacion(op){
-    num1=document.getElementById('principal').value
-    limpiar()
-    console.log(op)
-    operador=op
+
+function operacion(op) {
+    valorPantalla += `${op}`
+    document.getElementById('principal').value = valorPantalla;
+    numeros.push(Number.parseFloat(valorActual));
+    valorActual = '';
+
+    if (numeros.length > 1) {
+        if (operador == '+') {
+            resultado = Number.parseFloat(numeros[0]) + Number.parseFloat(numeros[1]);
+            numeros = [];
+            numeros.push(resultado);
+        }
+        if (operador == '-') {
+            resultado = Number.parseFloat(numeros[0]) - Number.parseFloat(numeros[1]);
+            numeros = [];
+            numeros.push(resultado);
+        }
+        if (operador == '*') {
+            resultado = Number.parseFloat(numeros[0]) * Number.parseFloat(numeros[1]);
+            numeros = [];
+            numeros.push(resultado);
+        }
+        if (operador == '/') {
+            resultado = Number.parseFloat(numeros[0]) / Number.parseFloat(numeros[1]);
+            numeros = [];
+            numeros.push(resultado);
+        }
+    }
+    operador = op;
 }
-var num2
-function igual(){
-    if(operador=='+'){
-        num2=document.getElementById('principal').value
-        limpiar()
-        document.getElementById('principal').value=Number(num1) + Number(num2)
+
+function igual() {
+
+    if (operador == '+') {
+        resultado = Number.parseFloat(numeros[0]) + Number.parseFloat(valorActual);
     }
-    if(operador=='-'){
-        num2=document.getElementById('principal').value
-        limpiar()
-        document.getElementById('principal').value=Number(num1) - Number(num2)
+    if (operador == '-') {
+        resultado = Number.parseFloat(numeros[0]) - Number.parseFloat(valorActual);
     }
-    if(operador=='*'){
-        num2=document.getElementById('principal').value
-        limpiar()
-        document.getElementById('principal').value=Number(num1) * Number(num2)
+    if (operador == '*') {
+        resultado = Number.parseFloat(numeros[0]) * Number.parseFloat(valorActual);
     }
-    if(operador=='/'){
-        num2=document.getElementById('principal').value
-        limpiar()
-        document.getElementById('principal').value=Number(num1) / Number(num2)
+    if (operador == '/') {
+        resultado = Number.parseFloat(numeros[0]) / Number.parseFloat(valorActual);
     }
+    if (!resultado) {
+        document.getElementById('principal').value = "Syntax Error";
+
+    } else {
+        document.getElementById('principal').value = resultado;
+    }
+    numeros = [];
+    resultado = '';
+    valorActual = '';
+    valorPantalla = '';
 }
